2805965e756b29bd763ee28684fe6db28d84f64c
Lubomir Kavetskiy <lubomirkavetskiy@gmail.com>
2023-07-24 18:21:54 +0300

    Instal date-fns

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,16 +1,46 @@
+import { Metadata } from 'next';
+
+import { prisma } from '@/db';
+
 import Header from './components/Header';
 import Form from './components/Form';
-import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Some dynamic temp title for rreserve',
 };
 
-export default function Reservation() {
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error('Restaurant not found');
+  }
+
+  return restaurant;
+};
+
+export default async function Reservation({
+  params: { slug },
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { date: string; partySize: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(slug);
+
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <Header />
+        <Header
+          image={restaurant!.main_image}
+          name={restaurant!.name}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
         <Form />
       </div>
     </div>
