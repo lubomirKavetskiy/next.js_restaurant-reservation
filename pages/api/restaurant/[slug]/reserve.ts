553a73334a881ba97d92e0345826ddba5ad3755d
Lubomir Kavetskiy <lubomirkavetskiy@gmail.com>
2023-07-24 14:48:47 +0300

    Determine the tables to book

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -67,7 +67,38 @@ export default async function handler(
     }
   });
 
-  return res.json({ searchTimeWithTables, tablesCount });
+  const tablesToBook: number[] = [];
+  let seatsRemaining = parseInt(partySize);
+
+  while (seatsRemaining > 0) {
+    if (seatsRemaining >= 3) {
+      if (tablesCount[4].length) {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining -= 4;
+      } else {
+        if (tablesCount[2].length) {
+          tablesToBook.push(tablesCount[2][0]);
+          tablesCount[2].shift();
+          seatsRemaining -= 2;
+        }
+      }
+    } else {
+      if (tablesCount[2].length) {
+        tablesToBook.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining -= 2;
+      } else {
+        if (tablesCount[4].length) {
+          tablesToBook.push(tablesCount[4][0]);
+          tablesCount[4].shift();
+          seatsRemaining -= 4;
+        }
+      }
+    }
+  }
+
+  return res.json({ searchTimeWithTables, tablesCount, tablesToBook });
 }
 
 //http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2025-01-31&time=15:00:00.000Z&partySize=2
