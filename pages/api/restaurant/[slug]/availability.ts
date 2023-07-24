3281db6336e2055feb7d821837961941c6dbf96d
Lubomir Kavetskiy <lubomirkavetskiy@gmail.com>
2023-07-24 15:37:18 +0300

    Create booking, lead it to the Tables

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -7,61 +7,63 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { slug, day, time, partySize } = req.query as {
-    slug: string;
-    day: string;
-    time: string;
-    partySize: string;
-  };
+  if (req.method === 'GET') {
+    const { slug, day, time, partySize } = req.query as {
+      slug: string;
+      day: string;
+      time: string;
+      partySize: string;
+    };
 
-  if (!day || !time || !partySize) {
-    return res.status(400).json({ message: 'Invalid data provided' });
-  }
+    if (!day || !time || !partySize) {
+      return res.status(400).json({ message: 'Invalid data provided' });
+    }
 
-  const restaurant = await prisma.restaurant.findUnique({
-    where: {
-      slug,
-    },
-    select: {
-      tables: true,
-      open_time: true,
-      close_time: true,
-    },
-  });
-
-  if (!restaurant) {
-    return res.status(400).json({ message: 'Invalid restaurant provided' });
-  }
-
-  const searchTimesWithTables = await findAvailableTables({
-    time,
-    day,
-    restaurant,
-    res,
-  });
-
-  if (!searchTimesWithTables) {
-    return res.status(400).json({ message: 'Invalid data provided' });
-  }
-
-  const availabilities = searchTimesWithTables
-    .map((item) => {
-      const sum = item.tables.reduce((acc, { seats }) => acc + seats, 0);
-
-      return { time: item.time, available: sum >= parseInt(partySize) };
-    })
-    .filter(({ time }) => {
-      const timeAfterOpeningHour =
-        new Date(`${day} ${time}`) >=
-        new Date(`${day} ${restaurant.open_time}`);
-      const timeBeforeClosingHour =
-        new Date(`${day} ${time}`) <=
-        new Date(`${day} ${restaurant.close_time}`);
-
-      return timeAfterOpeningHour && timeBeforeClosingHour;
+    const restaurant = await prisma.restaurant.findUnique({
+      where: {
+        slug,
+      },
+      select: {
+        tables: true,
+        open_time: true,
+        close_time: true,
+      },
     });
 
-  return res.status(200).json(availabilities);
+    if (!restaurant) {
+      return res.status(400).json({ message: 'Invalid restaurant provided' });
+    }
+
+    const searchTimesWithTables = await findAvailableTables({
+      time,
+      day,
+      restaurant,
+      res,
+    });
+
+    if (!searchTimesWithTables) {
+      return res.status(400).json({ message: 'Invalid data provided' });
+    }
+
+    const availabilities = searchTimesWithTables
+      .map((item) => {
+        const sum = item.tables.reduce((acc, { seats }) => acc + seats, 0);
+
+        return { time: item.time, available: sum >= parseInt(partySize) };
+      })
+      .filter(({ time }) => {
+        const timeAfterOpeningHour =
+          new Date(`${day} ${time}`) >=
+          new Date(`${day} ${restaurant.open_time}`);
+        const timeBeforeClosingHour =
+          new Date(`${day} ${time}`) <=
+          new Date(`${day} ${restaurant.close_time}`);
+
+        return timeAfterOpeningHour && timeBeforeClosingHour;
+      });
+
+    return res.status(200).json(availabilities);
+  }
 }
 
 //http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2025-01-31&time=15:00:00.000Z&partySize=2
--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -1,104 +1,140 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
 import { prisma } from '@/db';
 import findAvailableTables from '@/service/restuarant/findAvailableTables';
-import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { slug, day, time, partySize } = req.query as {
-    slug: string;
-    day: string;
-    time: string;
-    partySize: string;
-  };
+  if (req.method === 'POST') {
+    const { slug, day, time, partySize } = req.query as {
+      slug: string;
+      day: string;
+      time: string;
+      partySize: string;
+    };
 
-  const restaurant = await prisma.restaurant.findUnique({
-    where: {
-      slug,
-    },
-    select: {
-      tables: true,
-      open_time: true,
-      close_time: true,
-    },
-  });
+    const {
+      bookerEmail,
+      bookerPhone,
+      bookerFirstName,
+      bookerLastName,
+      bookerOccasion,
+      bookerRequest,
+    } = req.body;
 
-  if (!restaurant) {
-    return res.status(400).json({ message: 'Restaurant not found' });
-  }
+    const restaurant = await prisma.restaurant.findUnique({
+      where: {
+        slug,
+      },
+      select: {
+        tables: true,
+        open_time: true,
+        close_time: true,
+        id: true,
+      },
+    });
 
-  if (
-    new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
-    new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
-  ) {
-    return res
-      .status(400)
-      .json({ message: 'Restaurant is not open in this time' });
-  }
-
-  const searchTimesWithTables = await findAvailableTables({
-    time,
-    day,
-    restaurant,
-    res,
-  });
-
-  if (!searchTimesWithTables) {
-    return res.status(400).json({ message: 'Invalid data provided' });
-  }
-
-  const searchTimeWithTables = searchTimesWithTables.find(
-    ({ date }) =>
-      date.toISOString() === new Date(`${day}T${time}`).toISOString()
-  );
-
-  if (!searchTimeWithTables) {
-    return res.status(400).json({ message: 'No availability, cannot book' });
-  }
-
-  const tablesCount: { 2: number[]; 4: number[] } = { 2: [], 4: [] };
-
-  searchTimeWithTables.tables.forEach(({ seats, id }) => {
-    if (seats === 2) {
-      tablesCount[2].push(id);
-    } else {
-      tablesCount[4].push(id);
+    if (!restaurant) {
+      return res.status(400).json({ message: 'Restaurant not found' });
     }
-  });
 
-  const tablesToBook: number[] = [];
-  let seatsRemaining = parseInt(partySize);
+    if (
+      new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
+      new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
+    ) {
+      return res
+        .status(400)
+        .json({ message: 'Restaurant is not open in this time' });
+    }
 
-  while (seatsRemaining > 0) {
-    if (seatsRemaining >= 3) {
-      if (tablesCount[4].length) {
-        tablesToBook.push(tablesCount[4][0]);
-        tablesCount[4].shift();
-        seatsRemaining -= 4;
+    const searchTimesWithTables = await findAvailableTables({
+      time,
+      day,
+      restaurant,
+      res,
+    });
+
+    if (!searchTimesWithTables) {
+      return res.status(400).json({ message: 'Invalid data provided' });
+    }
+
+    const searchTimeWithTables = searchTimesWithTables.find(
+      ({ date }) =>
+        date.toISOString() === new Date(`${day}T${time}`).toISOString()
+    );
+
+    if (!searchTimeWithTables) {
+      return res.status(400).json({ message: 'No availability, cannot book' });
+    }
+
+    const tablesCount: { 2: number[]; 4: number[] } = { 2: [], 4: [] };
+
+    searchTimeWithTables.tables.forEach(({ seats, id }) => {
+      if (seats === 2) {
+        tablesCount[2].push(id);
+      } else {
+        tablesCount[4].push(id);
+      }
+    });
+
+    const tablesToBook: number[] = [];
+    let seatsRemaining = parseInt(partySize);
+
+    while (seatsRemaining > 0) {
+      if (seatsRemaining >= 3) {
+        if (tablesCount[4].length) {
+          tablesToBook.push(tablesCount[4][0]);
+          tablesCount[4].shift();
+          seatsRemaining -= 4;
+        } else {
+          if (tablesCount[2].length) {
+            tablesToBook.push(tablesCount[2][0]);
+            tablesCount[2].shift();
+            seatsRemaining -= 2;
+          }
+        }
       } else {
         if (tablesCount[2].length) {
           tablesToBook.push(tablesCount[2][0]);
           tablesCount[2].shift();
           seatsRemaining -= 2;
-        }
-      }
-    } else {
-      if (tablesCount[2].length) {
-        tablesToBook.push(tablesCount[2][0]);
-        tablesCount[2].shift();
-        seatsRemaining -= 2;
-      } else {
-        if (tablesCount[4].length) {
-          tablesToBook.push(tablesCount[4][0]);
-          tablesCount[4].shift();
-          seatsRemaining -= 4;
+        } else {
+          if (tablesCount[4].length) {
+            tablesToBook.push(tablesCount[4][0]);
+            tablesCount[4].shift();
+            seatsRemaining -= 4;
+          }
         }
       }
     }
-  }
 
-  return res.json({ searchTimeWithTables, tablesCount, tablesToBook });
+    const booking = await prisma.booking.create({
+      data: {
+        number_of_people: parseInt(partySize),
+        booking_time: new Date(`${day}T${time}`),
+        booker_email: bookerEmail,
+        booker_phone: bookerPhone,
+        booker_first_name: bookerFirstName,
+        booker_last_name: bookerLastName,
+        booker_occasion: bookerOccasion,
+        booker_request: bookerRequest,
+        restraurant_id: restaurant.id,
+      },
+    });
+
+    const bookingsOnTablesData = tablesToBook.map((tableId) => ({
+      booking_id: booking.id,
+      table_id: tableId,
+    }));
+
+    await prisma.bookingsOnTable.createMany({
+      data: bookingsOnTablesData,
+    });
+
+    return res.json(booking);
+  }
 }
 
 //http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2025-01-31&time=15:00:00.000Z&partySize=2
